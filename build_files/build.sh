9b66246e6844522bad1dc0e78916b2dbbf13dea0
Johanan Pacheco <email@johananpacheco.dev>
2026-05-04 16:48:06 +0200

    adding mangowm

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,9 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf install mangowm
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
