169608ad01ba8fb87b056ac4e1ec0fe91420c51d
Johanan Pacheco <email@johananpacheco.dev>
2026-05-04 17:05:52 +0200

    Try again with mango

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,6 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
-dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf6 install -y mangowm
 
 # Use a COPR Example:
