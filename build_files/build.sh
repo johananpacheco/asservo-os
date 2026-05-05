6663664d1f3a66dc24182af2a7b5e0f44c494a93
Johanan Pacheco <email@johananpacheco.dev>
2026-05-05 15:16:53 +0200

    Adding lug-helper (Star Citizen)

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,15 +11,21 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 
+# Niri and Noctalia
 dnf5 -y copr enable yalter/niri
 dnf5 -y install niri
 dnf5 -y copr disable yalter/niri
 dnf5 -y --enable-repo=terra install noctalia-shell
+# Ghostty
 dnf5 -y --enable-repo=terra install ghostty
-
+# Faugus Launcher
 dnf5 -y copr enable faugus/faugus-launcher
 dnf5 -y install faugus-launcher
 dnf5 -y copr disable faugus/faugus-launcher
+# Star Citizen
+dnf5 -y copr enable jackgreiner/lug-helper
+dnf5 -y install lug-helper
+dnf5 -y copr enable jackgreiner/lug-helper
 
 # Use a COPR Example:
 #
