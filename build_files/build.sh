e42142b031ae3436217f969b479a41d9fcc01ec7
Johanan Pacheco <email@johananpacheco.dev>
2026-05-05 08:21:13 +0200

    Removing 1password (doesn't work)

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,11 +16,6 @@ dnf5 -y copr disable yalter/niri
 dnf5 -y --enable-repo=terra install noctalia-shell
 dnf5 -y --enable-repo=terra install ghostty
 
-# 1password
-sudo rpm -y --import https://downloads.1password.com/linux/keys/1password.asc
-sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-dnf5 -y install 1password
-
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
