14b03182e69e610c685d404d4a6a8dd200df2264
Johanan Pacheco <email@johananpacheco.dev>
2026-05-05 14:18:23 +0200

    Cleanup

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,6 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 
-dnf5 -y install appimagelauncher
 dnf5 -y copr enable yalter/niri
 dnf5 -y install niri
 dnf5 -y copr disable yalter/niri
